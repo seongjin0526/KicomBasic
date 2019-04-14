f05c9b655a4f5162729d5e129d5648d11071786b
SteelJin <seongjin0526@naver.com>
2019-04-14 18:57:56 +0900

    edit: K_aes module

--- KicomBasic/k_aes.py
+++ KicomBasic/k_aes.py
@@ -5,20 +5,41 @@ from Crypto.Util.Padding import pad, unpad
 # AES Crypto Example #
 ######################
 
-key = b'1234567890123456'
-data = b'aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbcccccccccccccccc'
-
-# enc
-encCipher = AES.new(key, AES.MODE_CBC)
-iv = encCipher.iv
-ciphertext = encCipher.encrypt(pad(data, AES.block_size))
-print("etext:",ciphertext)
-
-# dec
-decCipher = AES.new(key, AES.MODE_CBC,iv=iv)
-ptext = unpad(decCipher.decrypt(ciphertext), AES.block_size)
 
 
-print("ptext:", ptext)
-print("eiv:", encCipher.iv)
-print("div:", decCipher.iv)
+class k_aes():
+    """
+    Enc/Decrypt Data using AES
+    Args:
+    key >= 16 bytes
+    
+    dec function need iv
+
+
+    Example:
+        #a = k_aes(b'0123456789012345')
+        #plain = b'aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbcccccccccccccccc'
+
+        #enc=a.enc(plain)
+        #print(enc[1])
+        #dec=a.dec(enc[0],enc[1])
+        #print(dec)
+    
+    """
+    def __init__(self, key):
+        self.key = key
+        
+        pass
+
+    def enc(self, data):
+        self.aes = AES.new(self.key, AES.MODE_CBC)
+        c_text = self.aes.encrypt(pad(data, AES.block_size))
+
+        return c_text, self.aes.iv
+
+    def dec(self, data, iv):
+        self.aes = AES.new(self.key, AES.MODE_CBC, iv)
+        p_text = unpad(self.aes.decrypt(data), AES.block_size)
+
+        return p_text
+    
